3afaed2b70c3f648d4ee880a5013513c71c7d512
Itz-Viggy <vigneshwar.sundararajan@gmail.com>
2025-08-05 16:55:42 -0400

    LiteDB

---
+++ SemantiCode.SearchEngine/LiteDbStorage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace SearchEngine
+{
+    public class LiteDbStorage : IDisposable
+    {
+        private readonly LiteDatabase _database;
+        private readonly ILiteCollection<SnippetDocument> _collection;
+
+        public LiteDbStorage(string databasePath = "codeindex.db")
+        {
+            if (string.IsNullOrWhiteSpace(databasePath))
+            {
+                throw new ArgumentException("Database path cannot be null or empty.", nameof(databasePath));
+            }
+            _database = new LiteDatabase(databasePath);
+            _collection = _database.GetCollection<SnippetDocument>("snippets");
+        }
+
+        public Task InsertManyAsync(IEnumerable<SnippetDocument> documents)
+        {
+            if (documents == null)
+            {
+                throw new ArgumentNullException(nameof(documents));
+            }
+            return Task.Run(() =>
+            {
+                _collection.InsertBulk(documents);
+            });
+
+        }
+
+        public void Dispose()
+        {
+            _database?.Dispose();
+        }
+    }
+}
