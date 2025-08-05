53c5301c7d738ec843f83d9238271cd7c3622567
Itz-Viggy <vigneshwar.sundararajan@gmail.com>
2025-08-05 17:07:01 -0400

    Index Repo

---
+++ SemantiCode.SearchEngine/IndexingRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine
+{
+    public interface IndexingRepo
+    {
+        Task<int> IndexRepoAsync(string repoPath);
+    }
+}
