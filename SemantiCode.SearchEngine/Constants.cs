a1022cf7cd4de11cc598e8c758b376993eca28c1
Itz-Viggy <vigneshwar.sundararajan@gmail.com>
2025-08-08 01:16:38 -0400

    Fixed the path problems

--- SemantiCode.SearchEngine/Constants.cs
+++ SemantiCode.SearchEngine/Constants.cs
@@ -1,9 +1,37 @@
-﻿namespace SearchEngine
+﻿using System.Reflection;
+
+namespace SearchEngine
 {
     internal static class Constants
     {
-        public const string ModelPath = "..\\SemantiCode.SearchEngine\\models\\bge-base.onnx";
-        public const string VocabPath = "..\\SemantiCode.SearchEngine\\models\\vocab.txt";
+        private static string GetModelPath(string fileName)
+        {
+            // Start from the current directory and look for the models folder
+            var currentDir = Directory.GetCurrentDirectory();
+            
+            // Try different possible paths
+            var possiblePaths = new[]
+            {
+                Path.Combine(currentDir, "models", fileName),
+                Path.Combine(currentDir, "..", "SemantiCode.SearchEngine", "models", fileName),
+                Path.Combine(currentDir, "..", "..", "SemantiCode.SearchEngine", "models", fileName),
+                Path.Combine(currentDir, "..", "..", "..", "SemantiCode.SearchEngine", "models", fileName),
+                Path.Combine(currentDir, "..", "..", "..", "..", "SemantiCode.SearchEngine", "models", fileName)
+            };
 
+            foreach (var path in possiblePaths)
+            {
+                if (File.Exists(path))
+                {
+                    return path;
+                }
+            }
+
+            // If file not found, throw a helpful exception
+            throw new FileNotFoundException($"Could not find {fileName} in any of the expected locations. Current directory: {currentDir}");
+        }
+
+        public static string ModelPath => GetModelPath("bge-base.onnx");
+        public static string VocabPath => GetModelPath("vocab.txt");
     }
 }
