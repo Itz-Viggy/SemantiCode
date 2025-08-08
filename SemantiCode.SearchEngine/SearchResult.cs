e4ee71cca9c54ce048f3d53a33e008b20b85f7bd
Itz-Viggy <vigneshwar.sundararajan@gmail.com>
2025-08-08 01:18:44 -0400

    Show the search result and score

---
+++ SemantiCode.SearchEngine/SearchResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine
+{
+    public class SearchResult
+    {
+        public SnippetDocument Document { get; set; }
+        public double SimilarityScore { get; set; }
+    }
+}
